1e0b81a93d9845c93a73f474eb1d229da1ca019c
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-29 15:59:51 -0300

    feat: crear barra de navegacion con sus estilos y rutas

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavbarComponent {
+
+}
