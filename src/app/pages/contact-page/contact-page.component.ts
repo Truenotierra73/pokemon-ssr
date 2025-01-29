eb809b56288e18b253f538b6946a083cf0017c7b
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-29 18:30:47 -0300

    feat: agregar titulo y meta tags

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,6 +7,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class ContactPageComponent {
+export default class ContactPageComponent implements OnInit {
+  private readonly title: Title = inject(Title);
+  private readonly meta: Meta = inject(Meta);
 
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page');
+    this.meta.updateTag({ name: 'description', content: 'Esta es mi Contact Page' });
+    this.meta.updateTag({ name: 'og:title', content: 'Contact Page' });
+    this.meta.updateTag({ name: 'keyboards', content: 'Hola,Mundo,Agustin,Bollati,Curso,Angular,PRO' });
+  }
 }
--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
@@ -6,6 +7,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent {
+export default class PricingPageComponent implements OnInit {
+  private readonly title: Title = inject(Title);
+  private readonly meta: Meta = inject(Meta);
 
+  ngOnInit(): void {
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({ name: 'description', content: 'Esta es mi Pricing Page' });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
+    this.meta.updateTag({ name: 'keyboards', content: 'Hola,Mundo,Agustin,Bollati,Curso,Angular,PRO' });
+  }
 }
