e3218a8a89d95d61f5a7c6feaee88339c1290a20
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-29 15:39:06 -0300

    feat: crear pagina de contacto

---
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-page',
+  imports: [],
+  templateUrl: './contact-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class ContactPageComponent {
+
+}
