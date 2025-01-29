6daf3dfdebcedc107620025df5922b21c5119a4b
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-29 15:39:24 -0300

    feat: crear pagina de precios

---
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-page',
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingPageComponent {
+
+}
