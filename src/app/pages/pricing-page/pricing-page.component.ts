8c0019c947e70f5f58bbde86188ac5921053e361
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-30 15:25:18 -0300

    feat: agregar platform id

--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -10,8 +11,13 @@ import { Meta, Title } from '@angular/platform-browser';
 export default class PricingPageComponent implements OnInit {
   private readonly title: Title = inject(Title);
   private readonly meta: Meta = inject(Meta);
+  private readonly platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //  document.title = 'Pricing Page';
+    // }
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({ name: 'description', content: 'Esta es mi Pricing Page' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
