b6eb42a202fffd793499a351b7c345732cc09b5e
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-30 18:17:16 -0300

    feat: agregar configuracion

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine, isMainModule } from '@angular/ssr/node';
 
+import { render } from '@netlify/angular-runtime/common-engine';
+
 import express from 'express';
 
 import { dirname, join, resolve } from 'node:path';
@@ -26,6 +28,17 @@ const commonEngine = new CommonEngine();
  * });
  * ```
  */
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine);
+}
 
 /**
  * Serve static files from /browser
